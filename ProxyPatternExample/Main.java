5823b7cd59b85b5e77054289d22fb47ba4aca0f2
DurgaAmirtha <durgaamirtha53@gmail.com>
2026-06-25 15:26:47 +0530

    Initial upload

---
+++ ProxyPatternExample/Main.java
@@ -0,0 +1,14 @@
+package ProxyPatternExample;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Image img1 = new ProxyImage("photo1.jpg");
+        Image img2 = new ProxyImage("photo2.jpg");
+
+        img1.display();
+        img1.display();
+
+        img2.display();
+    }
+}
